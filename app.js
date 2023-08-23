539f88579c98aa74365aff03ff4e4ef27802d464
Utku Cikmaz <utkucikmaz@gmail.com>
2023-08-23 12:49:45 +0200

    building layout and views with hbs

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ app.set("view engine", "hbs"); //sets HBS as the template engine
 
 // Home Page
 app.get("/", (req, res, next) => {
-    res.sendFile(__dirname + "/views/homepage.html");
+    res.render("homepage");
 });
 
 // Contact Page
 app.get("/contact", (req, res, next) => {
-    res.sendFile(__dirname + "/views/contact.html");
+    res.render("contact");
 });
 
 //Pizzas
@@ -22,19 +22,76 @@ app.get("/contact", (req, res, next) => {
 app.get("/pizzas/margarita", (req, res, next) => {
     //res.render(path, data)
 
-    const data = {
-        title: "Pizza Margarita",
+    const dataMargarita = {
+        title: "Margarita Pizza",
         price: 12,
+        recommendedDrink: "Beer",
+        imageFile: "pizza-margarita.jpg",
+        ingredients: [
+            {
+                ingredientName: "mozzarella",
+                calories: 400,
+            },
+            {
+                ingredientName: "tomato sauce",
+                calories: 200,
+            },
+            {
+                ingredientName: "basilicum",
+                calories: 30,
+            },
+        ],
     };
-    res.render("product", data);
+    res.render("product", dataMargarita);
 });
-//vegıe
+//veggie
 app.get("/pizzas/veggie", (req, res, next) => {
-    res.send("Veggie Pizza");
+    const dataVeggie = {
+        title: "Veggie Pizza",
+        price: 15,
+        recommendedDrink: "Power smoothie",
+        imageFile: "pizza-veggie.jpg",
+        ingredients: [
+            {
+                ingredientName: "cherry tomatoes",
+                calories: 80,
+            },
+            {
+                ingredientName: "basilicum",
+                calories: 30,
+            },
+            {
+                ingredientName: "olives",
+                calories: 300,
+            },
+        ],
+    };
+    res.render("product", dataVeggie);
 });
 //seafood
 app.get("/pizzas/seafood", (req, res, next) => {
-    res.send("Seafood Pizza");
+    const dataSeafood = {
+        title: "Seafood Pizza",
+        price: 20,
+        recommendedDrink: "White wine",
+        imageFile: "pizza-seafood.jpg",
+        ingredients: ["tomato sauce", "garlic", "prawn"],
+        ingredients: [
+            {
+                ingredientName: "tomato sauce",
+                calories: 200,
+            },
+            {
+                ingredientName: "garlic",
+                calories: 20,
+            },
+            {
+                ingredientName: "prawn",
+                calories: 350,
+            },
+        ],
+    };
+    res.render("product", dataSeafood);
 });
 
 app.listen(3000, () => console.log("My first app listening on port 3000! "));
