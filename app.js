fbaf82649dd0255ce2aa88b19d051c8c71d81c5e
Utku Cikmaz <utkucikmaz@gmail.com>
2023-08-23 11:02:39 +0200

    adding hbs and 3 pizzas

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 app.use(express.static("public"));
 
+app.set("views", __dirname + "/views"); //tells our Express app where to look for our views
+app.set("view engine", "hbs"); //sets HBS as the template engine
+
 // app.get(path, code)
 
 // Home Page
@@ -12,6 +15,26 @@ app.get("/", (req, res, next) => {
 // Contact Page
 app.get("/contact", (req, res, next) => {
     res.sendFile(__dirname + "/views/contact.html");
+});
+
+//Pizzas
+//Margarita
+app.get("/pizzas/margarita", (req, res, next) => {
+    //res.render(path, data)
+
+    const data = {
+        title: "Pizza Margarita",
+        price: 12,
+    };
+    res.render("product", data);
+});
+//vegıe
+app.get("/pizzas/veggie", (req, res, next) => {
+    res.send("Veggie Pizza");
+});
+//seafood
+app.get("/pizzas/seafood", (req, res, next) => {
+    res.send("Seafood Pizza");
 });
 
 app.listen(3000, () => console.log("My first app listening on port 3000! "));
